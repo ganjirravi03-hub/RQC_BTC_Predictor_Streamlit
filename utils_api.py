68cbfc97695d6853cbf096aab7f1446853882523
Ravi Ganjir <ganjirravi03@gmail.com>
2025-11-29 20:20:35 +0530

    Change BTC price fetch endpoint to CoinGecko

--- utils_api.py
+++ utils_api.py
@@ -1,20 +1,11 @@
-# utils_api.py
 import requests
 
 def fetch_btc_price():
-    """
-    Alternate endpoint that works on Streamlit Cloud.
-    """
     try:
-        url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-        headers = {"Cache-Control": "no-cache"}
-
-        response = requests.get(url, headers=headers, timeout=5)
-        if response.status_code == 200:
-            data = response.json()
-            return float(data["price"])
-        return None
-
+        url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+        response = requests.get(url, timeout=5)
+        data = response.json()
+        return float(data["bitcoin"]["usd"])
     except:
         return None
         
